dd8da076bbb3dda6f7df95cc34786088358a6947
Jose Manaure <jmanaure87@gmail.com>
2025-10-03 23:02:50 -0300

    re build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // importante si usas rutas relativas
+  base: './', // importante para rutas relativas
 })
